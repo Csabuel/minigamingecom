4c05c8e496ec31fdaca190db77114d9ad83d2945
Csabuel <119868774+Csabuel@users.noreply.github.com>
2023-06-22 16:30:11 +0200

    Update product.js

--- product.js
+++ product.js
@@ -1,6 +1,6 @@
 cpu = [{
     id:"id-01-001",
-    image:"/Products img/Intel-Core-i9-13th-Gen-i9-13900KF-Tetracosa-core-24-Core-3-GHz-Processor.png",
+    image:"Products img/Intel-Core-i9-13th-Gen-i9-13900KF-Tetracosa-core-24-Core-3-GHz-Processor.png",
     name:"Intel Core i9 (13th Gen) i9-13900KF Tetracosa-core (24 Core) 3 GHz Processor",
     priceCents: 57400, // price in cent
     condition:"New",
@@ -9,7 +9,7 @@ cpu = [{
     },
     {
     id:"id-01-002",
-    image:"/Products img/Intel-Core-i7-12700K-CPU-12-8P4E-Cores-up-to-5.png",
+    image:"Products img/Intel-Core-i7-12700K-CPU-12-8P4E-Cores-up-to-5.png",
     name:"Intel Core i7-12700K CPU 12 (8P+4E) Cores up to 5.0 GHz Unlocked LGA1700 (Intel 600 Series) 125W",
     priceCents: 41900, // price in cent
     condition:"New",
@@ -18,7 +18,7 @@ cpu = [{
     },
     {
     id:"id-01-003",
-    image:"/Products img/Intel-Core-i5-13th-Gen-i5-13600K-Tetradeca-core-14-Core-3.png",
+    image:"Products img/Intel-Core-i5-13th-Gen-i5-13600K-Tetradeca-core-14-Core-3.png",
     name:"Intel Core i5 (13th Gen) i5-13600K Tetradeca-core (14 Core) 3.50 GHz Processor",
     priceCents: 31999, // price in cent
     condition:"New",
@@ -27,7 +27,7 @@ cpu = [{
     },
     {
     id:"id-01-004",
-    image:"/Products img/AMD-Ryzen-9-7950X-Processor-16-core-32-Threads-Up-to-5.png",
+    image:"Products img/AMD-Ryzen-9-7950X-Processor-16-core-32-Threads-Up-to-5.png",
     name:"AMD Ryzen 9 7950X Processor 16-core 32 Threads Up to 5.7GHz AM5 125W AMD Radeon Graphics Desktop Processor 100-100000514WOF",
     priceCents: 59900, // price in cent
     condition:"New",
@@ -36,7 +36,7 @@ cpu = [{
     },
     {
     id:"id-01-005",
-    image:"/Products img/AMD-Ryzen-5-5600X-Processor-6-core-12-Threads-up-to-4.png",
+    image:"Products img/AMD-Ryzen-5-5600X-Processor-6-core-12-Threads-up-to-4.png",
     name:"AMD Ryzen 5 4500 Processor 6-core 12 Threads up to 4.1 GHz with Wraith Stealth Cooler AM4",
     priceCents: 8000, // price in cent
     condition:"New",
@@ -45,7 +45,7 @@ cpu = [{
     },
     {
     id:"id-01-006",
-    image:"/Products img/AMD-Ryzen-7-5800X-Processor-8-core-16-Threads-Up-to-4.png",
+    image:"Products img/AMD-Ryzen-7-5800X-Processor-8-core-16-Threads-Up-to-4.png",
     name:"AMD-Ryzen-7-5800X-Processor-8-core-16-Threads-Up-to-4",
     priceCents: 24900, // price in cent
     condition:"New",
@@ -56,7 +56,7 @@ cpu = [{
 
 cards =[{
     id:"id-02-001",
-    image:"/Products img/card1.png",
+    image:"Products img/card1.png",
     name:"MSI GeForce RTX 4070 GAMING X TRIO 12G Graphics Card G4070V3X12C",
     priceCents: 64999, // price in cent
     condition:"New",
@@ -65,7 +65,7 @@ cards =[{
     },
     {
     id:"id-02-002",
-    image:"/Products img/card2.png",
+    image:"Products img/card2.png",
     name:"PNY NVIDIA GeForce RTX 4080 GPU 16GB XLR8 Gaming VERTO EPIC-X RGB Triple Fan DLSS 3 Graphics Card",
     priceCents: 117999, // price in cent
     condition:"New",
@@ -78,7 +78,7 @@ cards =[{
 storage = [
     {
     id:"id-03-001",
-    image:"/Products img/storage1.png",
+    image:"Products img/storage1.png",
     name:"Samsung 1TB T7 USB 3.2 Portable External SSD MU-PC1T0T/AM",
     priceCents: 9999, // price in cent
     condition:"New",
@@ -87,7 +87,7 @@ storage = [
     },
     {
     id:"id-03-002",
-    image:"/Products img/storage2.png",
+    image:"Products img/storage2.png",
     name:"MSI SPATIUM M470 PCIe 4.0 NVMe M.2 1TB SM470N1TB SSD",
     priceCents: 8999, // price in cent
     condition:"New",
@@ -100,7 +100,7 @@ storage = [
 boards = [
     {
     id:"id-04-001",
-    image:"/Products img/board1.png",
+    image:"Products img/board1.png",
     name:"MSI MPG B550 GAMING PLUS DDR4 ATX B550G PLUS Gaming Motherboard",
     priceCents: 16999, // price in cent
     condition:"New",
@@ -109,7 +109,7 @@ boards = [
     },
     {
     id:"id-04-002",
-    image:"/Products img/board2.png",
+    image:"Products img/board2.png",
     name:"MSI B550 Tomahawk RGB GAMING Motherboard",
     priceCents: 21344, // price in cent
     condition:"New",
@@ -122,7 +122,7 @@ boards = [
 cooling =[
     {
     id:"id-05-001",
-    image:"/Products img/cooling1.png",
+    image:"Products img/cooling1.png",
     name:"MSI MAG Coreliquid 280R V2 Dual Fans 140-mm",
     priceCents: 12000, // price in cent
     condition:"New",
@@ -131,7 +131,7 @@ cooling =[
     },
     {
     id:"id-05-002",
-    image:"/Products img/cooling2.png",
+    image:"Products img/cooling2.png",
     name:"Cooler Master Hyper 212 Halo Black CPU Air Cooler",
     priceCents: 4900, // price in cent
     condition:"New",
@@ -144,7 +144,7 @@ cooling =[
 power = [
     {
     id:"id-06-001",
-    image:"/Products img/power1.png",
+    image:"Products img/power1.png",
     name:"EVGA SuperNOVA 650 GT 650W Gold Compact Power Supply",
     priceCents: 9799, // price in cent
     condition:"New",
@@ -153,7 +153,7 @@ power = [
     },
     {
     id:"id-06-002",
-    image:"/Products img/power2.png",
+    image:"Products img/power2.png",
     name:"CORSAIR RMe Series RM1000e Fully Modular 80 PLUS Gold ATX Power Supply",
     priceCents: 20000, // price in cent
     condition:"New",
@@ -166,7 +166,7 @@ power = [
 memory =[
     {
     id:"id-07-001",
-    image:"/Products img/memory1.png",
+    image:"Products img/memory1.png",
     name:"MSI SPATIUM M450 PCIe 4.0 NVMe M.2 1TB SM450N1TB SSD",
     priceCents: 5999, // price in cent
     condition:"New",
@@ -175,7 +175,7 @@ memory =[
     },
     {
     id:"id-07-002",
-    image:"/Products img/memory2.png",
+    image:"Products img/memory2.png",
     name:"PNY XLR8 Gaming REV RGB 16GB (2x8GB) DDR4 3200MHz CL16 Dual Channel Memory Upgrade MD16GK2D4320016X2RGB",
     priceCents: 7499, // price in cent
     condition:"New",
@@ -188,7 +188,7 @@ memory =[
 cases = [
     {
     id:"id-08-001",
-    image:"/Products img/case1.png",
+    image:"Products img/case1.png",
     name:"Cooler Master TD300 Mesh White Micro-ATX Tower Polygonal Mesh Front with Tempered Glass",
     priceCents: 9999, // price in cent
     condition:"New",
@@ -197,7 +197,7 @@ cases = [
     },
     {
     id:"id-08-002",
-    image:"/Products img/case2.png",
+    image:"Products img/case2.png",
     name:"Cooler Master TD300 Mesh Micro-ATX Tower Polygonal Mesh Front with Tempered Glass",
     priceCents: 10199, // price in cent
     condition:"New",
@@ -214,42 +214,42 @@ products = [
 productOptions =[
     {
     name:"CPUs & Processors",
-    image:"/Products img/productstype/PCCOMP_CPUProcessors_182x182_6UpVisNav_D.webp",
+    image:"Products img/productstype/PCCOMP_CPUProcessors_182x182_6UpVisNav_D.webp",
     id:'id-01'
     },
     {
     name:"Graphics Cards",
-    image:"/Products img/productstype/PCCOMP_VidCard_182x182_6UpVisNav_D.webp",
+    image:"Products img/productstype/PCCOMP_VidCard_182x182_6UpVisNav_D.webp",
     id:'id-02'
     },
     {
     name:"HDD & Storage",
-    image:"/Products img/productstype/PCCOMP_HDD_182x182_6UpVisNav_D.webp",
+    image:"Products img/productstype/PCCOMP_HDD_182x182_6UpVisNav_D.webp",
     id:'id-03'
     },
     {
     name:"Motherboards",
-    image:"/Products img/productstype/PCCOMP_MOBO_182x182_6UpVisNav_D.webp",
+    image:"Products img/productstype/PCCOMP_MOBO_182x182_6UpVisNav_D.webp",
     id:'id-04'
     },
     {
     name:"Cooling",
-    image:"/Products img/productstype/PCC_Cooling_182x182_6Up_Circle_VisNav_D.webp",
+    image:"Products img/productstype/PCC_Cooling_182x182_6Up_Circle_VisNav_D.webp",
     id:'id-05'
     },
     {
     name:"Power Supplies",
-    image:"/Products img/productstype/PCC_PowerSupplies_182x182_6Up_Circle_VisNav_D.webp",
+    image:"Products img/productstype/PCC_PowerSupplies_182x182_6Up_Circle_VisNav_D.webp",
     id:'id-06'
     },
     {
     name:"Memory",
-    image:"/Products img/productstype/PCCOMP_Memory_182x182_6UpVisNav_D.webp",
+    image:"Products img/productstype/PCCOMP_Memory_182x182_6UpVisNav_D.webp",
     id:'id-07'
     },
     {
     name:"Cases",
-    image:"/Products img/productstype/PCCOMP_Case_182x182_6UpVisNav_D.webp",
+    image:"Products img/productstype/PCCOMP_Case_182x182_6UpVisNav_D.webp",
     id:'id-08'
     },
 ];
